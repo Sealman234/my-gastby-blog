261243a868a529c3e08054a847c74cf8a9465469
Sealman <castle2668@gmail.com>
2022-03-15 19:09:14 +0800

    Finished Gatsby Tutorial Part 2

---
+++ src/components/layout.js
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import { Link } from 'gatsby';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  margin: auto;
+  max-width: 500px;
+  font-family: sans-serif;
+`;
+
+const Heading = styled.h1`
+  color: rebeccapurple;
+`;
+
+const NavLinks = styled.ul`
+  display: flex;
+  list-style: none;
+  padding-left: 0;
+`;
+
+const NavLinkItem = styled.li`
+  padding-right: 2rem;
+`;
+
+const NavLinkText = styled(Link)`
+  color: #000;
+`;
+
+const Layout = ({ pageTitle, children }) => {
+  return (
+    <Container>
+      <title>{pageTitle}</title>
+      <nav>
+        <NavLinks>
+          <NavLinkItem>
+            <NavLinkText to="/">Home</NavLinkText>
+          </NavLinkItem>
+          <NavLinkItem>
+            <NavLinkText to="/about">About</NavLinkText>
+          </NavLinkItem>
+        </NavLinks>
+      </nav>
+      <main>
+        <Heading>{pageTitle}</Heading>
+        {children}
+      </main>
+    </Container>
+  );
+};
+
+export default Layout;
