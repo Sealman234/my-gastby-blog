160aac10b9d097d2f1fa77aa0b1f740c9a6f5b29
Sealman <castle2668@gmail.com>
2022-03-15 23:36:46 +0800

    Finished Gatsby Tutorial Part 6

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
-import { graphql } from 'gatsby';
-import { MDXRenderer } from 'gatsby-plugin-mdx';
-import Layout from '../components/layout';
+import { graphql, Link } from 'gatsby';
+import Layout from '../../components/layout';
 
 const BlogPage = ({ data }) => {
   return (
@@ -9,9 +8,11 @@ const BlogPage = ({ data }) => {
       <ul>
         {data.allMdx.nodes.map((node) => (
           <article key={node.id}>
-            <h2>{node.frontmatter.title}</h2>
+            <h2>
+              <Link to={`/blog/${node.slug}`}>{node.frontmatter.title}</Link>
+            </h2>
             <p>Posted: {node.frontmatter.date}</p>
-            <MDXRenderer>{node.body}</MDXRenderer>
+            <p>Modified: {node.parent.modifiedTime}</p>
           </article>
         ))}
       </ul>
@@ -29,7 +30,7 @@ export const query = graphql`
           date(formatString: "YYYY-MM-DD")
         }
         id
-        body
+        slug
         parent {
           ... on File {
             modifiedTime(formatString: "YYYY-MM-DD")
