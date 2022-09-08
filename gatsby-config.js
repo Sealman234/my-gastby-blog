c565466d5e93d4947b8890a5a80cb9d544c5fdc9
Sealman <castle2668@gmail.com>
2022-09-08 14:34:46 +0800

    feat: update PWA setting

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,11 +52,39 @@ module.exports = {
         background_color: '#fff',
         theme_color: '#333',
         display: 'standalone',
-        // Automatic mode configuration
-        icon: 'src/images/icon.png',
+        icon: 'src/images/favicon.png', // This path is relative to the root of the pre-rendered site
+        icons: [
+          // Paths relative to the static folder
+          {
+            src: `favicon/favicon-256.png`,
+            sizes: `256x256`,
+            type: `image/png`,
+          },
+          {
+            src: `favicon/favicon-128.png`,
+            sizes: `128x128`,
+            type: `image/png`,
+          },
+          {
+            src: `favicon/favicon-64.png`,
+            sizes: `64x64`,
+            type: `image/png`,
+          },
+          {
+            src: `favicon/favicon-32.png`,
+            sizes: `32x32`,
+            type: `image/png`,
+          },
+          {
+            src: `favicon/favicon-maskable.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+            purpose: `maskable`,
+          },
+        ],
+        cache_busting_mode: 'none',
       },
     },
-    'gatsby-plugin-offline',
     {
       resolve: `gatsby-plugin-offline`,
       options: {
